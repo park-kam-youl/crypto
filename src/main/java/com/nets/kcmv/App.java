63c264f72812d87794b60836614536fd9b1e7155
unknown <kamten@nets.co.kr>
2025-07-16 10:11:52 +0900

    Initial commit: Maven library skeleton

---
+++ src/main/java/com/nets/kcmv/App.java
@@ -0,0 +1,13 @@
+package com.nets.kcmv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
